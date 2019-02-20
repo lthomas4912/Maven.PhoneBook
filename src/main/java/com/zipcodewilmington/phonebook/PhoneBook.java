88808739765f46e1d3bb03a2f0b136b79f283c80
Leah <leah29726@gmail.com>
2019-02-20 10:58:30 -0500

    Started test

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -4,4 +4,9 @@ package com.zipcodewilmington.phonebook;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    public String getName() {
+    }
+
+    public String getNumber() {
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,28 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+
+    public void addTest(String name, String phoneNumber){
+
+        //Given
+        String expectedName = "";
+        String expectedNumber = "";
+
+        //When
+        PhoneBook phoneBook = new PhoneBook();
+
+        //Then
+        String actualName = phoneBook.getName();
+        String acutalNumber = phoneBook.getNumber();
+
+        Assert.
+    }
 }
