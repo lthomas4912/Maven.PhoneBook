fed0c7d5c8093a8cf92ec492f50e11d7987429ed
Leah <leah29726@gmail.com>
2019-02-21 21:58:30 -0500

    Add and remove test complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,12 +1,42 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-    public String getName() {
+
+    private Map<String,String>phoneBook = new TreeMap<>();
+
+
+    public void add(String name, String number){
+
+        phoneBook.put(name, number);
     }
 
-    public String getNumber() {
+    public void remove(String name) {
+        phoneBook.remove(name);
     }
+
+
+    public int size() {
+        return phoneBook.size();
+    }
+
+
+    public Map<String, String> getPhoneBook() {
+        return phoneBook;
+    }
+
+    public void setPhoneBook(Map<String, String> phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public String lookup(String name) {
+        String number = phoneBook.get(name);
+        return number;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -3,26 +3,74 @@ package com.zipcodewilmington.phonebook;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
 
+    PhoneBook phoneBook = new PhoneBook();
+
+
+
     @Test
 
-    public void addTest(String name, String phoneNumber){
+    public void addTest(){
 
         //Given
-        String expectedName = "";
-        String expectedNumber = "";
+
 
         //When
-        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Sheila", "3028578983");
+
+
 
         //Then
-        String actualName = phoneBook.getName();
-        String acutalNumber = phoneBook.getNumber();
+        int expected = phoneBook.size();
+        int actual = 1;
 
-        Assert.
+        Assert.assertEquals(expected,actual);
+
+
     }
+
+    @Test
+
+    public void removeTest(){
+        //Given
+        phoneBook.add("Sheila", "3028578983");
+
+        //When
+        phoneBook.remove("Sheila");
+
+
+        //Then
+        int expected = phoneBook.size();
+        int actual = 0;
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+
+    public void lookUpTest(){
+        //Given
+        String name = "Sheila";
+        phoneBook.add("", "");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("");
+
+        //When
+
+
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+    }
+
+
+
 }
